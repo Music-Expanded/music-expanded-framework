53c6d3fa0473710a0968bcacf82244b1e959b9dc
Daniel Caaz <dcaazy@gmail.com>
2022-09-01 21:21:33 -0700

    Remove SoundDef in favor of a Modextension

--- Source/Def/SoundDef.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace MusicExpanded
-{
-    public class SoundDef : Verse.SoundDef
-    {
-        public List<Verse.SoundDef> replaces = new List<Verse.SoundDef>();
-    }
-}
---
+++ Source/ModExtension/ReplacesSounds.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Verse;
+
+namespace MusicExpanded.ModExtension
+{
+    public class ReplacesSounds : DefModExtension
+    {
+        public List<SoundDef> sounds = new List<SoundDef>();
+    }
+}
